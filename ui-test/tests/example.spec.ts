078afdceeb7eb6f136a43fc131c94df0e5fe6d17
up1 <somkiat.p@gmail.com>
2024-05-08 16:12:36 +0700

    Fix test

--- ui-test/tests/example.spec.ts
+++ ui-test/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5000');
+  await page.goto('http://web:5000');
 
   // Expect a title "to contain" a substring.
   // await expect(page).toBeTitle('Example Domain');
